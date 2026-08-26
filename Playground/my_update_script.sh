bfae82039f160632ecb9bac58bb2452765b3d576
Nallans <nallans.code@gmail.com>
2026-08-26 22:26:37 +0200

    Adding autoremove to my_update_script + changing displayed message

--- Playground/my_update_script.sh
+++ Playground/my_update_script.sh
@@ -1,7 +1,8 @@
 sudo apt update
 sudo apt upgrade
+sudo apt autoremove
 sudo fwupdmgr get-updates
 sudo fwupdmgr get-upgrades
-echo "UPDATES COMPLETED. AUTODESTRUCT IN 10 SECONDS."
+echo "UPDATES COMPLETED. SELF-DESTRUCT IN 10 SECONDS."
 sleep 10
 systemctl reboot
